39a09591e45e136c154945b4ccf5284cbf4c031d
AliveKiwi <prateekjais.dev@gmail.com>
2024-02-14 18:27:48 +0530

    2. Passing props to component & Destructuring

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,17 @@
-function ProfileCard() {
-  return <div>Profile Card!!!</div>;
+// Advance Destructuring
+function ProfileCard({ title, handle }) {
+  // const title = props.title
+  // const handle = props.handle
+
+  // Destructuring
+  // const { title, handle } = props;
+
+  return (
+    <div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  );
 }
 
 export default ProfileCard;
